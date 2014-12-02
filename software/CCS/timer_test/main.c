3452b8d2ec173f3b654d574bf6d1e587029998ba
Rohit Ramesh <Rohit507@gmail.com>
2014-12-02 16:37:32 -0500

    Lots of changes, including getting "Hello World!" across the gap.

--- software/CCS/timer_test/main.c
+++ software/CCS/timer_test/main.c
@@ -13,6 +13,14 @@
  * 		(docs/slau272c.pdf or http://www.ti.com/lit/ug/slau272c/slau272c.pdf)
  *
  */
+
+// ---------- Absurd Macro Hackery -------------
+
+volatile int j = 0;
+
+#define T1TIME ((60 * 20)/2)
+#define T2TIME ((60 * 50)/2)
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;					// Stop watchdog timer
 
@@ -23,8 +31,8 @@ int main(void) {
     CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;       	// set all dividers to 0
 	
     // Pre-load timer registers with count values
-    TB0CCR0 = 60;								// 60 counts * 1s/6e6counts = 10us
-    TB0CCR1 = 600;								// 600 counts * 1s/6e6counts = 100us
+    TB0CCR0 = T1TIME;								// 60 counts * 1s/6e6counts = 10us
+    TB0CCR1 = T2TIME;								// 600 counts * 1s/6e6counts = 100us
 
     TB0CCTL0 = CCIE;							// Set TB0.0 to compare, interrupt enabled
     TB0CCTL1 = CCIE;							// Set TB0.1 to compare, interrupt enabled
@@ -35,10 +43,61 @@ int main(void) {
     P2DIR |= (BIT0 + BIT1);
     P2OUT &= ~(BIT0 + BIT1);
 
+	P2DIR |= (1 << 2);						// Set LEDG to output direction
+	P1DIR |= (1 << 6);					    // Set LEDB to output direction
+	P1DIR |= (1 << 7);					    // Set LEDR to output direction
+
     __enable_interrupt();
 
-    while(1);
 
+
+    while(1){
+
+    }
+
+}
+
+volatile int i = 0;
+int c = 0;
+const int t = 300;
+
+const char* message = "Hello World!";
+
+void state_update(){
+	static int s = 0;
+	static int n = 0;
+	static int b = 0;
+
+	if(s == 0){
+		j = 1;
+		c = 8 * t;
+		s = 1;
+	} else if (s == 1){
+		j = 0;
+		c = t;
+		s = 2;
+	} else if (s == 2){
+		j = 1;
+		c = t;
+		b = 0;
+		s = 3;
+	} else if (s == 3){
+		j = 1 & (message[n] >> b);
+		c = t;
+		b++;
+		if(b == 8){
+			n++;
+			if(!message[n]){
+				n = 0;
+			}
+			b = 0;
+			s = 4;
+		}
+	} else if (s == 4){
+		j = 0;
+		c = 2 * t;
+		s = 0;
+	}
 }
 
 #pragma vector=TIMERB0_VECTOR
@@ -46,9 +105,15 @@ __interrupt void TIMER_B0 (void) {
 
 	TB0CCTL0 &= ~CCIFG;
 
-	P2OUT ^= BIT0;
-
-	TB0CCR0 += 60;
+	if(j){
+		P2OUT ^= BIT0;
+	}
+	i++;
+	if(i >= c){
+		state_update();
+		i = 0;
+	}
+	TB0CCR0 += T1TIME;
 }
 
 #pragma vector=TIMERB1_VECTOR
@@ -56,7 +121,15 @@ __interrupt void TIMER_B1 (void) {
 
 	TB0CCTL1 &= ~CCIFG;
 
-	P2OUT ^= BIT1;
+	if(!j){
+		P2OUT ^= BIT0;
+	}
 
-	TB0CCR1 += 600;
+	if(j){
+		P2OUT |= (1 << 2);	// Set LEDG on
+	} else {
+		P2OUT &= ~(1 << 2);	// Set LEDG off
+	}
+
+	TB0CCR1 += T2TIME;
 }
--- software/GNURadio/top_block.py
+++ software/GNURadio/top_block.py
@@ -2,12 +2,14 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Sun Nov 30 15:45:48 2014
+# Generated: Tue Dec  2 16:35:21 2014
 ##################################################
 
 from PyQt4 import Qt
 from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio import uhd
@@ -49,14 +51,26 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self._update_period_config = ConfigParser.ConfigParser()
+        self._update_period_config.read("default")
+        try: update_period = self._update_period_config.getfloat("main", "key")
+        except: update_period = 0.001
+        self.update_period = update_period
         self.sub_carrier_freq = sub_carrier_freq = 1000
-        self.send_gain = send_gain = 40
-        self.samp_rate = samp_rate = 10000
+        self.send_gain = send_gain = 0
+        self.samp_rate = samp_rate = 20000
+        self.rms_alpha = rms_alpha = 0.01
         self.recieve_gain = recieve_gain = 1
+        self._fft_size_config = ConfigParser.ConfigParser()
+        self._fft_size_config.read("default")
+        try: fft_size = self._fft_size_config.getfloat("main", "key")
+        except: fft_size = 8192
+        self.fft_size = fft_size
+        self.data_rate = data_rate = 0.1
         self._carrier_freq_config = ConfigParser.ConfigParser()
         self._carrier_freq_config.read("default")
         try: carrier_freq = self._carrier_freq_config.getfloat("main", "key")
-        except: carrier_freq = 915000000
+        except: carrier_freq = 916000000
         self.carrier_freq = carrier_freq
         self._USRP_IP_config = ConfigParser.ConfigParser()
         self._USRP_IP_config.read("default")
@@ -151,23 +165,83 @@ class top_block(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink_0.set_center_freq(carrier_freq, 0)
         self.uhd_usrp_sink_0.set_gain(send_gain, 0)
         self.uhd_usrp_sink_0.set_bandwidth(samp_rate, 0)
-        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
-        	1024, #size
+        self._rms_alpha_tool_bar = Qt.QToolBar(self)
+        self._rms_alpha_tool_bar.addWidget(Qt.QLabel("rms_alpha"+": "))
+        self._rms_alpha_line_edit = Qt.QLineEdit(str(self.rms_alpha))
+        self._rms_alpha_tool_bar.addWidget(self._rms_alpha_line_edit)
+        self._rms_alpha_line_edit.returnPressed.connect(
+        	lambda: self.set_rms_alpha(eng_notation.str_to_num(self._rms_alpha_line_edit.text().toAscii())))
+        self.tabs_layout_1.addWidget(self._rms_alpha_tool_bar)
+        self.qtgui_waterfall_sink_x_1 = qtgui.waterfall_sink_c(
+        	8096, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	carrier_freq, #fc
+        	samp_rate, #bw
         	"QT GUI Plot", #name
-        	1 #number of inputs
+                1 #number of inputs
         )
-        self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
-        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
-        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, sub_carrier_freq, 1, 0)
+        self.qtgui_waterfall_sink_x_1.set_update_time(0.01)
+        self._qtgui_waterfall_sink_x_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_1.pyqwidget(), Qt.QWidget)
+        self.tabs_layout_0.addWidget(self._qtgui_waterfall_sink_x_1_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	302400, #size
+        	samp_rate, #samp_rate
+        	"QT GUI Plot", #name
+        	3 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	carrier_freq, #fc
+        	samp_rate, #bw
+        	"QT GUI Plot", #name
+        	3 #number of inputs
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.tabs_layout_0.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._data_rate_tool_bar = Qt.QToolBar(self)
+        self._data_rate_tool_bar.addWidget(Qt.QLabel("Data Rate (Hz)"+": "))
+        self._data_rate_line_edit = Qt.QLineEdit(str(self.data_rate))
+        self._data_rate_tool_bar.addWidget(self._data_rate_line_edit)
+        self._data_rate_line_edit.returnPressed.connect(
+        	lambda: self.set_data_rate(eng_notation.str_to_num(self._data_rate_line_edit.text().toAscii())))
+        self.tabs_layout_1.addWidget(self._data_rate_tool_bar)
+        self.blocks_sub_xx_0 = blocks.sub_ff(1)
+        self.blocks_rms_xx_1 = blocks.rms_cf(0.1)
+        self.blocks_rms_xx_0 = blocks.rms_cf(0.1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((-500, ))
+        self.band_pass_filter_0_0 = filter.fir_filter_ccf(1, firdes.band_pass(
+        	1, samp_rate, 1175, 1225, 25, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0 = filter.fir_filter_ccf(1, firdes.band_pass(
+        	1, samp_rate, 475, 525, 25, firdes.WIN_HAMMING, 6.76))
+        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, sub_carrier_freq, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.uhd_usrp_source_0_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0_0, 0), (self.band_pass_filter_0_0, 0))
+        self.connect((self.uhd_usrp_source_0_0, 0), (self.qtgui_freq_sink_x_0, 2))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_rms_xx_0, 0))
+        self.connect((self.band_pass_filter_0_0, 0), (self.blocks_rms_xx_1, 0))
+        self.connect((self.blocks_rms_xx_1, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_rms_xx_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.blocks_rms_xx_1, 0), (self.blocks_sub_xx_0, 0))
+        self.connect((self.blocks_rms_xx_0, 0), (self.blocks_sub_xx_0, 1))
+        self.connect((self.band_pass_filter_0, 0), (self.qtgui_freq_sink_x_0, 1))
+        self.connect((self.band_pass_filter_0_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_sub_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_time_sink_x_0, 2))
+        self.connect((self.uhd_usrp_source_0_0, 0), (self.qtgui_waterfall_sink_x_1, 0))
 
 
 # QT sink close method reimplementation
@@ -176,6 +250,12 @@ class top_block(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+    def get_update_period(self):
+        return self.update_period
+
+    def set_update_period(self, update_period):
+        self.update_period = update_period
+
     def get_sub_carrier_freq(self):
         return self.sub_carrier_freq
 
@@ -183,7 +263,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.sub_carrier_freq = sub_carrier_freq
         self._sub_carrier_freq_counter.setValue(self.sub_carrier_freq)
         self._sub_carrier_freq_slider.setValue(self.sub_carrier_freq)
-        self.analog_sig_source_x_0.set_frequency(self.sub_carrier_freq)
+        self.analog_sig_source_x_1.set_frequency(self.sub_carrier_freq)
 
     def get_send_gain(self):
         return self.send_gain
@@ -201,8 +281,20 @@ class top_block(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_sink_0.set_bandwidth(self.samp_rate, 0)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 475, 525, 25, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0.set_taps(firdes.band_pass(1, self.samp_rate, 1175, 1225, 25, firdes.WIN_HAMMING, 6.76))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.carrier_freq, self.samp_rate)
         self.uhd_usrp_source_0_0.set_samp_rate(self.samp_rate)
+        self.qtgui_waterfall_sink_x_1.set_frequency_range(self.carrier_freq, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_rms_alpha(self):
+        return self.rms_alpha
+
+    def set_rms_alpha(self, rms_alpha):
+        self.rms_alpha = rms_alpha
+        self._rms_alpha_line_edit.setText(eng_notation.num_to_str(self.rms_alpha))
 
     def get_recieve_gain(self):
         return self.recieve_gain
@@ -213,13 +305,28 @@ class top_block(gr.top_block, Qt.QWidget):
         self._recieve_gain_slider.setValue(self.recieve_gain)
         self.uhd_usrp_source_0_0.set_gain(self.recieve_gain, 0)
 
+    def get_fft_size(self):
+        return self.fft_size
+
+    def set_fft_size(self, fft_size):
+        self.fft_size = fft_size
+
+    def get_data_rate(self):
+        return self.data_rate
+
+    def set_data_rate(self, data_rate):
+        self.data_rate = data_rate
+        self._data_rate_line_edit.setText(eng_notation.num_to_str(self.data_rate))
+
     def get_carrier_freq(self):
         return self.carrier_freq
 
     def set_carrier_freq(self, carrier_freq):
         self.carrier_freq = carrier_freq
         self.uhd_usrp_sink_0.set_center_freq(self.carrier_freq, 0)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.carrier_freq, self.samp_rate)
         self.uhd_usrp_source_0_0.set_center_freq(self.carrier_freq, 0)
+        self.qtgui_waterfall_sink_x_1.set_frequency_range(self.carrier_freq, self.samp_rate)
 
     def get_USRP_IP(self):
         return self.USRP_IP
