81e407be43b0e70942798596e7440b20f8b9040e
Brad Campbell <bradjc5@gmail.com>
2015-12-05 22:46:57 -0500

    publish to emoncms script

---
+++ data_collection/emoncms/powerblade_emoncms.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+
+var noble = require('noble');
+var request = require('request');
+
+var POWERBLADE_MANUF_ID = 0x4908;
+
+var INPUT_URL = 'http://inductor.eecs.umich.edu/input/post.json';
+var APIKEY = 'none';
+
+if (process.argv.length == 3) {
+    APIKEY = process.argv[2];
+} else {
+    console.log('Must include emoncms APIKEY as first argument.');
+    process.exit(1);
+}
+
+
+// {BLE Address: Most recent sequence number} for each PowerBlade
+var powerblade_sequences = {};
+
+noble.on('stateChange', function(state) {
+    if (state === 'poweredOn') {
+        console.log("Starting scan...");
+        noble.startScanning([], true);
+    } else {
+        console.log('Scanning error: ' + state);
+        noble.stopScanning();
+    }
+});
+
+noble.on('error', function (err) {
+    console.log(err);
+})
+
+noble.on('discover', function (peripheral) {
+
+    // Advertisement found. Get the manufacturer ID, if any
+    var advertisement = peripheral.advertisement;
+    var manufacturer_id = 0;
+    if (typeof advertisement.manufacturerData !== undefined && advertisement.manufacturerData
+            && advertisement.manufacturerData.length >= 2) {
+        manufacturer_id = advertisement.manufacturerData.readUIntLE(0,2);
+    }
+
+    if (manufacturer_id == POWERBLADE_MANUF_ID) {
+        // Found a PowerBlade
+        var address   = peripheral.address;
+        var data      = advertisement.manufacturerData.slice(2);
+        var recv_time = (new Date).getTime()/1000;
+
+        // check length of data
+        if (data.length < 19) {
+            console.log("ERROR: Bad PowerBlade packet");
+            return;
+        }
+
+        // check software version number
+        var version_num = data.readUIntBE(0,1);
+        if (version_num == 1) {
+
+            // check for duplicate advertisements
+            var sequence_num = data.readUIntBE(1,4);
+            if (!(address in powerblade_sequences)) {
+                powerblade_sequences[address] = -1;
+            }
+            if (powerblade_sequences[address] == sequence_num) {
+                // duplicate advertisement. Don't display
+                return;
+            }
+            powerblade_sequences[address] = sequence_num;
+
+            // parse fields from advertisement
+            var pscale             = data.readUIntBE(5,2);
+            var vscale             = data.readUIntBE(7,1);
+            var whscale            = data.readUIntBE(8,1);
+            var v_rms_raw          = data.readUIntBE(9,1);
+            var real_power_raw     = data.readUIntBE(10,2);
+            var apparent_power_raw = data.readUIntBE(12,2);
+            var watt_hours_raw     = data.readUIntBE(14,4);
+            var flags              = data.readUIntBE(18,1);
+
+            // calculate scaling values
+            var volt_scale  = vscale / 50;
+            var power_scale = (pscale & 0x0FFF) * Math.pow(10,-1*((pscale & 0xF000) >> 12));
+            var wh_shift    = whscale;
+
+            // scale measurements from PowerBlade
+            var v_rms      = v_rms_raw*volt_scale;
+            var real_power = real_power_raw*power_scale;
+            var app_power  = apparent_power_raw*power_scale;
+            if (volt_scale > 0) {
+              var watt_hours = (watt_hours_raw << wh_shift)*(power_scale/3600);
+            } else {
+              var watt_hours = watt_hours_raw;
+            }
+            var pf = real_power / app_power;
+
+            // Display data for logging purposes
+            console.log(new Date() +
+                ' PowerBlade[' + address +'] #' +
+                sequence_num + ' ' +
+                v_rms.toFixed(2)+'V '  + 
+                 real_power.toFixed(2) + 'W ' +
+                 app_power.toFixed(2) + 'VA ' +
+                watt_hours.toFixed(2) + 'kWh ' +
+                pf.toFixed(2));
+
+            // Generate the object we send to emoncms
+            var post_data = {
+                seq:              sequence_num,
+                voltage_rms:      v_rms.toFixed(1),
+                power_w:          real_power.toFixed(1),
+                apparent_power_w: app_power.toFixed(1),
+                energy_kwh:       watt_hours.toFixed(1),
+                pf:               pf.toFixed(1)
+            };
+
+            // Check if we have an address to identify this PowerBlade
+            if (address != 'unknown') {
+                // Send to emoncms
+                var url = INPUT_URL + '?node=' + address + '&json=' + JSON.stringify(post_data) + '&apikey=' + APIKEY;
+                request.post(url);
+            } else {
+                console.log('ERROR - could not send to emoncms because address is unknown.');
+            }
+
+        } else {
+            // This version of PowerBlade is not supported by this script
+            console.log('ERROR: PowerBlade version ' + version_num + ' unsupported.');
+        }
+        
+    }
+});
+
