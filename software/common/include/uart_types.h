438561cdc9070a59f7a83959dd1379fec27311b3
Sam DeBruin <samuel.debruin@skyspecs.com>
2015-12-14 17:26:09 -0500

    added uart types header file

---
+++ software/common/include/uart_types.h
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+
+#ifndef UART_TYPES_H_
+#define UART_TYPES_H_
+
+/**************************************************************************
+   UART TYPES SECTION
+ **************************************************************************/
+#define CALIB_STATE		0x10
+#define GT_WATTS		0x11
+#define GET_CUROFF		0x12
+#define SET_CUROFF		0x13
+#define GET_PSCALE		0x14
+#define SET_PSCALE		0x15
+#define GET_VSCALE		0x16
+#define SET_VSCALE		0x17
+#define GET_WHSCALE		0x18
+#define SET_WHSCALE		0x19
+#define SET_SEQ			0x1A
+#define START_SAMDATA	0x20
+#define CONT_SAMDATA	0x21
+#define UART_NAK		0xFF
+
+/**************************************************************************
+   STATE MACHINE SECTION
+ **************************************************************************/
+typedef enum {
+	TXNORMAL,
+	TXSAMPLE,
+} tx_type_t;
+
+#endif
