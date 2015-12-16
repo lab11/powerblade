7893e4bc2fdf94387a94a965a2a1b74e3e74402f
Branden Ghena <brghena@umich.edu>
2015-12-16 01:34:39 -0500

    Raw sample collection service
    Sends uart command to MSP430 to request raw ADC samples of voltage and
    current. Data is passed to the nRF which makes it available over the
    service in 504-byte chunks.
    
    Code not yet tested with an MSP430, but the BLE side seems to work

--- software/nrf51822/apps/powerblade/ble_config.h
+++ software/nrf51822/apps/powerblade/ble_config.h
@@ -4,6 +4,7 @@
 #define APP_COMPANY_IDENTIFIER			0x4908
 
 #include "simple_ble.h"
+#include "uart_types.h"
 #include "powerblade.h"
 
 // Intervals for advertising and connections
@@ -17,26 +18,42 @@ static const simple_ble_config_t ble_config = {
 };
 
 typedef struct ble_app_s {
-    simple_ble_app_t*           simple_ble_app;
+    simple_ble_app_t* simple_ble_app;
 
     // service for powerblade calibration
-    uint16_t                    calibrate_service_handle;
+    uint16_t calibrate_service_handle;
 
         // characteristic to start calibration mode
-        ble_gatts_char_handles_t    calibrate_char_begin_handle;
-        bool                        begin_calibration;
+        ble_gatts_char_handles_t calibrate_char_begin_handle;
+        bool                     begin_calibration;
 
         // characteristic to specify ground truth wattage for calibration
-        ble_gatts_char_handles_t    calibrate_char_watts_handle;
-        int16_t                     ground_truth_watts;
+        ble_gatts_char_handles_t calibrate_char_watts_handle;
+        int16_t                  ground_truth_watts;
 
         // characteristic to inform user to move to next calibration wattage
-        ble_gatts_char_handles_t    calibrate_char_next_handle;
-        bool                        next_wattage;
+        ble_gatts_char_handles_t calibrate_char_next_handle;
+        bool                     next_wattage;
 
         // characteristic to inform user calibration is complete
-        ble_gatts_char_handles_t    calibrate_char_done_handle;
-        bool                        done_calibrating;
+        ble_gatts_char_handles_t calibrate_char_done_handle;
+        bool                     done_calibrating;
+
+    // service for sample data collection
+    uint16_t rawSample_service_handle;
+
+        // characteristic to start raw sample collection
+        ble_gatts_char_handles_t rawSample_char_begin_handle;
+        bool                     begin_rawSample;
+
+        // characteristic to provide raw samples to users
+        ble_gatts_char_handles_t rawSample_char_data_handle;
+        uint8_t                  raw_sample_data[SAMDATA_MAX_LEN];
+
+        // characteristic to indicate new data is available
+        ble_gatts_char_handles_t rawSample_char_status_handle;
+        uint8_t                  rawSample_status;
+
 } ble_app_t;
 
 #endif
