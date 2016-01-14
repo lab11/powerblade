9fae56ba3a7e66c3d860e09af4c6324a40df4c31
Sam DeBruin <samuel.debruin@skyspecs.com>
2016-01-14 13:33:14 -0500

    updated the uart protocol to include ioff and voff

--- software/common/include/uart_types.h
+++ software/common/include/uart_types.h
@@ -8,17 +8,19 @@
 /**************************************************************************
    UART TYPES SECTION
  **************************************************************************/
-#define CALIB_STATE     0x10
-#define GT_WATTS        0x11
-#define GET_CUROFF      0x12
-#define SET_CUROFF      0x13
-#define GET_PSCALE      0x14
-#define SET_PSCALE      0x15
-#define GET_VSCALE      0x16
-#define SET_VSCALE      0x17
-#define GET_WHSCALE     0x18
-#define SET_WHSCALE     0x19
-#define SET_SEQ         0x1A
+#define GET_VOFF     	0x10
+#define SET_VOFF        0x11
+#define GET_IOFF		0x12
+#define SET_IOFF		0x13
+#define GET_CUROFF      0x14
+#define SET_CUROFF      0x15
+#define GET_PSCALE      0x16
+#define SET_PSCALE      0x17
+#define GET_VSCALE      0x18
+#define SET_VSCALE      0x19
+#define GET_WHSCALE     0x1A
+#define SET_WHSCALE     0x1B
+#define SET_SEQ         0x1C
 #define START_SAMDATA   0x20
 #define CONT_SAMDATA    0x21
 #define DONE_SAMDATA    0x22
