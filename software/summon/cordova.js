26e6340ed1a17ea317681517ac8f76a47e58d178
Branden Ghena <brghena@umich.edu>
2016-02-17 22:11:38 -0500

    Move loading to earlier in the process
    It looks like this is actually correctly initializing cordova for
    different platforms!

--- software/summon/cordova.js
+++ software/summon/cordova.js
@@ -1,14 +1,17 @@
 // select which cordova_*.js to load based on the platform
 if (navigator.platform.startsWith("iP")) {
     // iOS (iPhone, iPad, iPod)
+    console.log("Loading iOS-specific cordova");
     $.getScript('cordova_ios.js', load_after);
 } else {
     // android or bust
+    console.log("Loading android-specific cordova");
     $.getScript('cordova_android.js', load_after);
 }
 
 // don't load the user's index.js until cordova is loaded
 function load_after () {
+    console.log("Loading user's index.js file");
     $.getScript('js/index.js');
 }
 
