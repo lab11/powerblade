9b5534ce5838fb88990237a3e2111e084c6fd1fd
Sam DeBruin <sdebruin@umich.edu>
2016-03-20 22:52:14 -0400

    split basic data file into update table seperately

--- data/sql/basic_data.sh
+++ data/sql/basic_data.sh
@@ -58,10 +58,6 @@ esac
 shift # past argument or value
 done
 
-#echo
-#read -s -p 'Enter MySql Password: ' pwd
-#echo
-
 if [[ -n "${DURTIME+1}" ]]; then
 	echo "Query over ${DURTIME} minutes"
 else
@@ -77,46 +73,8 @@ else
 	echo "Ending now"
 fi
 
-echo "Generating BLEES binary data"
-mysql --login-path=resistor whisperwood -e "INSERT INTO blees_binary 
-SELECT t1.*,t2.avg_lux,
-CASE WHEN t1.lux > t2.avg_lux THEN 1 ELSE 0 END AS 'onoff'
-FROM blees_test t1
-JOIN (
-SELECT deviceMAC,AVG(lux) AS avg_lux FROM blees_test GROUP BY deviceMAC
-) t2
-ON t1.deviceMAC=t2.deviceMAC
-WHERE t1.id>(SELECT max(id) FROM blees_binary)
-ORDER BY t1.deviceMAC, t1.timestamp;"
-
-echo "Generating BLEES power data"
-mysql --login-path=resistor whisperwood -e "INSERT INTO blees_power
-SELECT t1.id, t1.gatewayMAC, t1.deviceMAC, t1.timestamp,
-CASE WHEN t1.onoff=1 THEN t2.power ELSE 0 END AS power
-FROM blees_binary t1
-JOIN blees_light_lookup t2
-ON t2.deviceMAC=t1.deviceMAC
-WHERE t1.id>(SELECT max(id) FROM blees_power)
-ORDER BY t1.deviceMAC;"
-
-echo "Inserting PowerBlade & BLEES data"
-mysql --login-path=resistor whisperwood -e "INSERT INTO overall_power (pb_id, gatewayMAC, deviceMAC, power, timestamp)
-SELECT id, gatewayMAC, deviceMAC, power, timestamp
-FROM powerblade_test t1 WHERE t1.id>(SELECT max(pb_id) FROM overall_power) OR
-0=(SELECT count(pb_id) FROM overall_power)
-ORDER BY timestamp ASC;
-INSERT INTO overall_power (bl_id, gatewayMAC, deviceMAC, power, timestamp)
-SELECT id, gatewayMAC, deviceMAC, power, timestamp
-FROM blees_power t2 WHERE t2.id>(SELECT max(bl_id) FROM overall_power) OR
-0=(SELECT count(bl_id) FROM overall_power)
-ORDER BY timestamp ASC;"
-
-# echo "Inserting BLEES data"
-# mysql --login-path=resistor whisperwood -e "INSERT INTO overall_power (bl_id, gatewayMAC, deviceMAC, power, timestamp)
-# SELECT id, gatewayMAC, deviceMAC, power, timestamp
-# FROM blees_power t2 WHERE t2.id>(SELECT max(bl_id) FROM overall_power) OR
-# 0=(SELECT count(bl_id) FROM overall_power)
-# ORDER BY timestamp ASC;"
+# Update Tables
+./update_tables.sh
 
 # This is really shameless... im sorry
 DEVICELIST_TEMP=""
@@ -221,7 +179,6 @@ if [[ -n "${OUTFILE+1}" ]]; then
 	fi
 fi
 
-#rm temp.plt
 rm temp.eps
 
 
