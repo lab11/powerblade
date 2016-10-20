5c985869e74b287756fb9955c1e12546b6bbd0c6
Sam DeBruin <sdebruin@umich.edu>
2016-10-20 08:23:01 -0400

    lots of changes - added yesterday and today features

--- sql/sql_status.py
+++ sql/sql_status.py
@@ -5,63 +5,11 @@ import MySQLdb
 import yagmail
 from datetime import datetime, timedelta
 import sys
+from os.path import expanduser
 
 STATUS_OK = "<font color=\"green\"><b>OK!</b></font>"
 STATUS_DOWN = "<font color=\"orange\"><b>Down</b></font>"
 STATUS_NOT_FOUND = "</td><td></td><td><font color=\"red\"><b>Not Found</b></font>"
-
-f = open('/etc/swarm-gateway/powerblade-aws.conf', 'r')
-password = 0
-for line in f:
-	lineList = line.strip('\n').split(' = ')
-	if(lineList[0] == 'sql_pw'):
-		password = lineList[1]
-f.close()
-
-if(password == 0):
-	print("Unable to find password")
-	exit()
-
-longrun = 1
-pb_error_list = []
-gw_error_list = []
-if len(sys.argv) > 1:
-	if(sys.argv[1] == 'short'):
-		print("Running PowerBlade Deployment Status Script - short check")
-		email_body = ['<!DOCTYPE html><html><body><h2> PowerBlade Deployment Status Email - New Error Found</h2>']
-		f = open('/tmp/powerblade-error.log', 'r')
-		for line in f:
-			try:
-				deviceMAC, error = line.strip('\n').split(',')
-				pb_error_list.append((deviceMAC, error))
-			except:
-				pass
-		f.close()
-		f = open('/tmp/gateway-error.log', 'r')
-		for line in f:
-			try:
-				gatewayMAC, error = line.strip('\n').split(',')
-				gw_error_list.append((gatewayMAC, error))
-			except:
-				pass
-		f.close()
-		longrun = 0
-	elif(sys.argv[1] == 'daily'):
-		print("Running PowerBlade Deployment Status Script - daily run")
-		email_body = ['<!DOCTYPE html><html><body><h2> PowerBlade Deployment Status Email - Full Update</h2>']
-	else:
-		print("Unknown parameter")
-		# TODO: send error email
-		exit()
-else:
-	print("Need to run with either \'short\' or \'daily\'")
-	exit()
-
-#print(pb_error_list)
-#exit()
-email_body.append('<style>\n\t.bottom-three {\n\t\tmargin-bottom: 3cm;\n\t}\n</style>')
-email_body.append('<p class=\"bottom-three\">Script start time: ' + str(datetime.utcnow()) + '</p>')
-email_end = '</table></body></html>'
 
 def chop_microseconds(delta):
     return delta - timedelta(microseconds=delta.microseconds)
@@ -75,26 +23,37 @@ def print_header(col1, col2):
 	else:
 		email_body.append("<tr><td colspan=\"5\">&nbsp</td></tr>")
 	email_body.append("<tr><td><b>" + col1 + "</b></td><td><b>" + col2 + \
-		"</b></td><td><b>Last Seen</b></td><td><b>Offtime</b></td><td><b>Status</b></td></tr>")
+		"</b></td><td><b>Last Seen</b></td><td><b>Offtime</b></td>" \
+		"<td><b>Status</b></td><td><b>Count</b></td></tr>")
 
-def print_row(name, specifier, time_now, maxTime, status):
+def print_row(name, specifier, time_now, maxTime, status, count):
 	email_body.append("<tr><td>" + str(name) + "</td><td>" + str(specifier) + "</td><td>" + str(maxTime) + \
-		"</td><td>" + str(chop_microseconds(time_now - maxTime)) + "</td><td>" + status + "</td></tr>")
+		"</td><td>" + str(chop_microseconds(time_now - maxTime)) + "</td><td>" + status + "</td><td>" + count + "</td></tr>")
 
 def print_error(name, specifier):
 	email_body.append("<tr><td>" + str(name) + "</td><td>" + str(specifier) + "</td><td>" + STATUS_NOT_FOUND + "</td></tr>")
 
-def check_list(activelist, timeslist, errorlist, outfile, col1, col2):
+def print_list(error_list, col1, col2):
+	if(!longrun):
+		return 0	# Not supposed to happen
+
+	print_header(col1, col2)
+
+	for devname in error_list:
+		this_errorlist = error_list[devname]
+		print_row(devname, this_errorlist[0], this_errorlist[1], this_errorlist[2], this_errorlist[3], this_errorlist[4])
+
+def check_list(activelist, timeslist, yest_errorlist, today_errorlist, outfile, col1, col2):
+	if(longrun):
+		return 0	# Not supposed to happen
+
 	time_now = datetime.utcnow()
 
-	if(longrun == 1):
-		print_header(col1, col2)
+	print_header(col1, col2)
 
 	new_errors = 0
-	if(longrun != 1):
-		errors = open(outfile, 'w')
 
-	for devname, specifier in activelist:
+	for devname, specifier in activelist:	# Specifier is either location or permanent
 		if(specifier is not None):	# Location actually exists
 			try:
 				maxTime = [item for item in timeslist if item[0] == devname][0][1]
@@ -103,78 +62,180 @@ def check_list(activelist, timeslist, errorlist, outfile, col1, col2):
 				else:
 					status = STATUS_OK
 
-				if(longrun == 1):
-					print_row(devname, specifier, time_now, maxTime, status)
-					new_errors += 1
-				else:
-					if(status != STATUS_OK):
-						finditem = [item for item in errorlist if item[0] == devname and item[1] == status]
-						if(finditem):
-							pass
-						else:
-							if(new_errors == 0):
-								print_header(col1, col2)
+				if(status != STATUS_OK):
+					try:
+						# Check if this error has happened today, and if so increment count
+						today_errorlist[devname][4] += 1
+					except:
+						# New device/error for today, add new element
+						today_errorlist[devname] = [specifier, time_now, maxTime, status, 1]
+						try:
+							# Check if this error also didnt happen yesterday, in which case it would have already been sent
+							yest_errorlist[devname]
+						except:
+							# Error did not happen yesterday or today, OK to send
 							new_errors += 1
-							print_row(devname, specifier, time_now, maxTime, status)
-						errors.write(str(devname) + ',' + str(status) + '\n')
-				
+							print_row(devname, specifier, time_now, maxTime, status, 1)
 			except IndexError:
 				# print("Error: gateway not found - " + str(gateway))
-				if(longrun == 1):
-					print_error(devname, specifier)
+				pass
+				#if(longrun == 1):
+				#	print_error(devname, specifier)
+
+	errors = open(outfile, 'w')
+	for devname in today_errorlist:
+		errors.write(devname)
+		for field in today_errorlist[devname]:
+			errors.write(',' + field)
+		errors.write('\n')
 
 	return new_errors
 
-# Set up connection
-aws_login = mylogin.get_login_info('aws')
-aws_db = MySQLdb.connect(aws_login['host'], aws_login['user'], aws_login['passwd'], 'powerblade')
-aws_c = aws_db.cursor()
+def read_file(error_list, listType, day):
+	infile = open('/tmp/' + listType + '-error-' + day.strftime("%Y-%m-%d") + '.log', 'r')
+	for line in infile:
+		deviceMAC, specifier, time_error, avgErrorTime, error, count = line.strip('\n').split(',')
+		error_list[deviceMAC] = [specifier, time_error, avgErrorTime, error, count]
+	infile.close()
 
-# Query for list of gateways that should be active (inf_gw_lookup where now is between dates)
-aws_c.execute('select gatewayMAC, location from ' \
-	'(select t1.* from inf_gw_lookup t1 where t1.id = ' \
-	'(select max(t2.id) from inf_gw_lookup t2 where t1.gatewayMAC=t2.gatewayMAC)) t1 ' \
-	'where ((utc_timestamp between startTime and endTime) or ' \
-	'(utc_timestamp > startTime and endTime is null) or ' \
-	'(utc_timestamp < endTime and startTime is null)) order by GatewayMAC desc;')
-# aws_c.execute('select gatewayMAC, location from inf_gw_lookup where ((utc_timestamp between startTime and ' \
-# 	'endTime) or (utc_timestamp > startTime and endTime is null) or (utc_timestamp < endTime and startTime is null)) order by GatewayMAC desc')
-gateway_active = aws_c.fetchall()
 
-# Query for list of PowerBlades that should be active (inf_pb_lookup where now is between dates)
-aws_c.execute('select deviceMAC, permanent from ' \
-	'(select t1.* from inf_pb_lookup t1 where t1.id = ' \
-	'(select max(t2.id) from inf_pb_lookup t2 where t1.deviceMAC=t2.deviceMAC)) t1 ' \
-	'where ((utc_timestamp between startTime and endTime) or ' \
-	'(utc_timestamp > startTime and endTime is null) or ' \
-	'(utc_timestamp < endTime and startTime is null)) order by deviceMAC desc;')
-#aws_c.execute('select deviceMAC, permanent from inf_pb_lookup where utc_timestamp between startTime and endTime')
-pb_active = aws_c.fetchall()
+# Start of execution
 
-# Query for most recent time seeing each gateway
-aws_c.execute('select t1.gatewaymac, (select t2.timestamp from dat_powerblade t2 where t2.id=max(t1.id)) as maxTime ' \
-	'from dat_powerblade t1 group by t1.gatewaymac')
-gateway_times = aws_c.fetchall()
 
-# Query for most recent time seeing each PowerBlade
-aws_c.execute('select t1.deviceMAC, (select t2.timestamp from dat_powerblade t2 where t2.id=max(t1.id)) as maxTime ' \
-	'from dat_powerblade t1 group by t1.deviceMAC')
-pb_times = aws_c.fetchall()
+# Get the home directory, log path
+home = expanduser("~")
+logpath = home + '/log/'
 
-total_errors = 0
+# Get database parameters
+f = open('/etc/swarm-gateway/powerblade-aws.conf', 'r')
+password = 0
+for line in f:
+	lineList = line.strip('\n').split(' = ')
+	if(lineList[0] == 'sql_pw'):
+		password = lineList[1]
+f.close()
 
-total_errors += check_list(gateway_active, gateway_times, gw_error_list, '/tmp/gateway-error.log', "GatewayMAC", "Location")
-total_errors += check_list(pb_active, pb_times, pb_error_list, '/tmp/powerblade-error.log', "DeviceMAC", "Permanent")
+if(password == 0):
+	print("Unable to find password")
+	exit()
 
-email_body.append(email_end)
+# Read in the yesterday and today files
+# If this is a long run the entire yesterday file is emailed
+# If this is a short run the error is only emailed if not in the yesterday file
+today = datetime.now()
+yesterday = today - timedelta(1)
 
-if(total_errors > 0):
-	print("Sending results via email")
-	if(longrun == 1):
-		yagmail.SMTP('powerblade.lab11@gmail.com', password).send('powerblade@umich.edu', 'Re: PowerBlade Deployment Status Email', email_body)
+pb_yest_list = {}
+read_file(pb_yest_list, 'powerblade', yesterday)
+# pb_f_yesterday = open('/tmp/powerblade-error-' + yesterday.strftime("%Y-%m-%d") + '.log', 'r')
+# for line in pb_f_yesterday:
+# 	deviceMAC, specifier, time_error, avgErrorTime, error, count = line.strip('\n').split(',')
+# 	pb_yest_list[deviceMAC] = [error, count]
+# pb_f_yesterday.close()
+
+gw_yest_list = {}
+read_file(gw_yest_list, 'gateway', yesterday)
+# gw_f_yesterday = open('/tmp/gateway-error-' + yesterday.strftime("%Y-%m-%d") + '.log', 'r')
+# for line in gw_f_yesterday:
+# 	deviceMAC, specifier, time_error, avgErrorTime, error, count = line.strip('\n').split(',')
+# 	gw_yest_list[deviceMAC] = [error, count]
+# gw_f_yesterday.close()
+
+pb_today_list = {}
+read_file(pb_today_list, 'powerblade', today)
+# pb_f_today = open('/tmp/powerblade-error-' + today.strftime("%Y-%m-%d") + '.log', 'r')
+# for line in pb_f_today:
+# 	deviceMAC, specifier, time_error, avgErrorTime, error, count = line.strip('\n').split(',')
+# 	pb_today_list[deviceMAC] = [error, count]
+# pb_f_today.close()
+
+gw_today_list = {}
+read_file(gw_today_list, 'gateway', today)
+# gw_f_today = open('/tmp/gateway-error-' + today.strftime("%Y-%m-%d") + '.log', 'r')
+# for line in gw_f_today:
+# 	deviceMAC, specifier, time_error, avgErrorTime, error, count = line.strip('\n').split(',')
+# 	gw_today_list[deviceMAC] = [error, count]
+# gw_f_today.close()
+
+
+longrun = True
+if len(sys.argv) > 1:
+	if(sys.argv[1] == 'short'):
+		print("Running PowerBlade Deployment Status Script - short check")
+		email_body = ['<!DOCTYPE html><html><body><h2> PowerBlade Deployment Status Email - New Error Found</h2>']
+		longrun = False
+	elif(sys.argv[1] == 'daily'):
+		print("Running PowerBlade Deployment Status Script - daily run")
+		email_body = ['<!DOCTYPE html><html><body><h2> PowerBlade Deployment Status Email - Full Update</h2>']
 	else:
-		yagmail.SMTP('powerblade.lab11@gmail.com', password).send('powerblade@umich.edu', 'Re: PowerBlade Deployment New Device Error', email_body)
+		print("Unknown parameter")
+		# TODO: send error email
+		exit()
 else:
-	print("No errors, no email")
+	print("Need to run with either \'short\' or \'daily\'")
+	exit()
+
+email_body.append('<style>\n\t.bottom-three {\n\t\tmargin-bottom: 3cm;\n\t}\n</style>')
+email_body.append('<p class=\"bottom-three\">Script start time: ' + str(datetime.utcnow()) + '</p>')
+email_end = '</table></body></html>'
+
+# If this is a daily run the database is not connected, instead only the yesterday file is sent
+# If a shortrun, the database is connected and new errors are found
+if(longrun):
+	print_list(gw_yest_list, "GatewayMAC", "Location")
+	print_list(pb_yest_list, "DeviceMAC", "Permanent")
+	print("Sending results via email")
+	email_body.append(email_end)
+	yagmail.SMTP('powerblade.lab11@gmail.com', password).send('powerblade@umich.edu', 'Re: PowerBlade Deployment Status Email', email_body)
+else:
+	# Set up connection
+	aws_login = mylogin.get_login_info('aws')
+	aws_db = MySQLdb.connect(aws_login['host'], aws_login['user'], aws_login['passwd'], 'powerblade')
+	aws_c = aws_db.cursor()
+
+	# Query for list of gateways that should be active (inf_gw_lookup where now is between dates)
+	aws_c.execute('select gatewayMAC, location from ' \
+		'(select t1.* from inf_gw_lookup t1 where t1.id = ' \
+		'(select max(t2.id) from inf_gw_lookup t2 where t1.gatewayMAC=t2.gatewayMAC)) t1 ' \
+		'where ((utc_timestamp between startTime and endTime) or ' \
+		'(utc_timestamp > startTime and endTime is null) or ' \
+		'(utc_timestamp < endTime and startTime is null)) order by GatewayMAC desc;')
+	# aws_c.execute('select gatewayMAC, location from inf_gw_lookup where ((utc_timestamp between startTime and ' \
+	# 	'endTime) or (utc_timestamp > startTime and endTime is null) or (utc_timestamp < endTime and startTime is null)) order by GatewayMAC desc')
+	gateway_active = aws_c.fetchall()
+
+	# Query for list of PowerBlades that should be active (inf_pb_lookup where now is between dates)
+	aws_c.execute('select deviceMAC, permanent from ' \
+		'(select t1.* from inf_pb_lookup t1 where t1.id = ' \
+		'(select max(t2.id) from inf_pb_lookup t2 where t1.deviceMAC=t2.deviceMAC)) t1 ' \
+		'where ((utc_timestamp between startTime and endTime) or ' \
+		'(utc_timestamp > startTime and endTime is null) or ' \
+		'(utc_timestamp < endTime and startTime is null)) order by deviceMAC desc;')
+	#aws_c.execute('select deviceMAC, permanent from inf_pb_lookup where utc_timestamp between startTime and endTime')
+	pb_active = aws_c.fetchall()
+
+	# Query for most recent time seeing each gateway
+	aws_c.execute('select t1.gatewaymac, (select t2.timestamp from dat_powerblade t2 where t2.id=max(t1.id)) as maxTime ' \
+		'from dat_powerblade t1 group by t1.gatewaymac')
+	gateway_times = aws_c.fetchall()
+
+	# Query for most recent time seeing each PowerBlade
+	aws_c.execute('select t1.deviceMAC, (select t2.timestamp from dat_powerblade t2 where t2.id=max(t1.id)) as maxTime ' \
+		'from dat_powerblade t1 group by t1.deviceMAC')
+	pb_times = aws_c.fetchall()
+
+	total_errors = 0
+
+	check_list(activelist, timeslist, yest_errorlist, today_errorlist, outfile, col1, col2):
+	total_errors += check_list(gateway_active, gateway_times, gw_yest_list, gw_today_list, '/tmp/gateway-error-' + today.strftime("%Y-%m-%d") + '.log', "GatewayMAC", "Location")
+	total_errors += check_list(pb_active, pb_times, pb_yest_list, pb_today_list, '/tmp/powerblade-error-' + today.strftime("%Y-%m-%d") + '.log', "DeviceMAC", "Permanent")
+
+	email_body.append(email_end)
+
+	if(total_errors > 0):
+		print("Sending results via email")
+		yagmail.SMTP('powerblade.lab11@gmail.com', password).send('powerblade@umich.edu', 'Re: PowerBlade Deployment New Device Error', email_body)
+	else:
+		print("No errors, no email")
 
 
