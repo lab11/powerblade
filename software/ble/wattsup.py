4dcc6c6b629c668e79f0c4460912a9fdde12046c
Sam DeBruin <sdebruin@umich.edu>
2016-11-29 10:49:34 -0500

    fixed errors with watts up script printing

--- software/ble/wattsup.py
+++ software/ble/wattsup.py
@@ -81,7 +81,10 @@ directory = os.environ['PB_DATA']
 
 # Prepare to write data
 if(len(sys.argv) > 1):
-	outfile = directory + '/' + sys.argv[1] + ".dat"
+	outfile = directory + '/' + sys.argv[1]
+	if(len(sys.argv) > 2):
+		outfile += "_" + sys.argv[2]
+	outfile += ".dat"	
 else:
 	outfile = directory + '/wattsup.dat'
 
