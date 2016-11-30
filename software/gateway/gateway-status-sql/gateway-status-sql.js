709f5ea9529f8c44ff96944eb2a0cc134604ead6
Sam DeBruin <sdebruin@umich.edu>
2016-11-30 12:13:33 -0500

    fixed two errors with sql

--- software/gateway/gateway-status-sql/gateway-status-sql.js
+++ software/gateway/gateway-status-sql/gateway-status-sql.js
@@ -80,13 +80,13 @@ var j = schedule.scheduleJob('*/1 * * * *', function() {
         public_ip = ip;
     
         // Post all three values to SQL
-        var loadQuery = 'INSERT INTO inf_gw_status (gatewayMAC, gatewayIP, publicIP) VALUES (' + gateway_mac + ', INET_ATON(' + gateway_ip + '), INET_ATON(' + public_ip + '));';
+        var loadQuery = 'INSERT INTO inf_gw_status (gatewayMAC, gatewayIP, publicIP) VALUES (\'' + gateway_mac + '\', INET_ATON(\'' + gateway_ip + '\'), INET_ATON(\'' + public_ip + '\'));';
         console.log(loadQuery);
 
         db_connection.query(loadQuery, function(err, rows, fields) {
             if (err) throw err;
             if(debug) {
-                console.log("Done uploading status for " + gateway_mac + " at " + gateway_ip ", " + public_ip);
+                console.log("Done uploading status for " + gateway_mac + " at " + gateway_ip + ", " + public_ip);
             }
         });
     });
