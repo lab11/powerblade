28019073bd1937fdeafde143b2338a5d8f6c6ddd
Sam DeBruin <sdebruin@umich.edu>
2016-12-22 15:12:43 -0500

    added location to powerblade lookup upload

--- sql/upload_status_form.sql
+++ sql/upload_status_form.sql
@@ -1,5 +1,5 @@
 
-load data local infile 'C:/Users/Sam/Downloads/pb.csv' into table inf_pb_lookup fields terminated by ',' (deviceMAC, deviceName, category, deviceType, permanent, startTime, endTime);
+load data local infile 'C:/Users/Sam/Downloads/pb.csv' into table inf_pb_lookup fields terminated by ',' (deviceMAC, deviceName, category, deviceType, location, permanent, startTime, endTime);
 load data local infile 'C:/Users/Sam/Downloads/gw.csv' into table inf_gw_lookup fields terminated by ',' (gatewayMAC, location, startTime, endTime);
 
 
