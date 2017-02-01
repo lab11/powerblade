a503290800ebea7d16defec70091381b97bfe737
Sam DeBruin <sam@endectra.com>
2017-02-01 17:47:13 -0500

    commission script almost ready to go

--- software/commissioning/commission.js
+++ software/commissioning/commission.js
@@ -22,8 +22,9 @@
 
 var fs = require('fs');
 var ini = require('ini');
+var readline = require('readline');
 var mysql = require('mysql');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 
 // AWS Configuration
 try {
@@ -57,11 +58,15 @@ else {
 	});	
 }
 
+/*************************************************************/
 // Step #1: Determine the device's ID, or assign a new ID
+/*************************************************************/
+console.log("Checking for Device ID");
 var device_id;
 //if(/* Check if device has ID */) {
 if(false) {
 	//device_id = // Device ID;
+	console.log("Device ID Found: " + device_id + "\n");
 	program_nrf();
 }
 else {
@@ -69,16 +74,60 @@ else {
 	db_connection.query(id_query, function(err, rows, fields) {
 		if (err) throw err;
 		device_id = rows[0].newMac;
+		console.log("New Device, Assigning to Device ID " + device_id);
+		console.log("This device will require a new address sticker\n")
 		program_nrf();
 	});
 }
 
+/*************************************************************/
 // Step #2: Program the nrf
+/*************************************************************/
 function program_nrf() {
-	console.log("Device ID: " + device_id);
-	var child = exec("make flash ID=c0:98:e5:70:01:5d -C $PB_ROOT/software/nrf51822/apps/powerblade", function(error, stdout, stderr) {
-		
+	console.log("Programming nRF51822 with Device ID: " + device_id);
+	var child = exec("make flash ID=c0:98:e5:70:01:5d -C " + process.env.PB_ROOT + "/software/nrf51822/apps/powerblade", function(error, stdout, stderr) {
+		if (error) throw error;
+		console.log("Done programming nrf51822\n")
+		switch_to_msp();
 	});
+}
+
+/*************************************************************/
+// Step #3: Program the MSP430
+/*************************************************************/
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function switch_to_msp() {
+	console.log("Preparing to program the MSP430. Move to the TI debugger (10 pin interface).");
+	rl.question('Push enter when ready', function(answer) {
+		program_msp();
+	});
+}
+
+function program_msp() {
+	console.log("\nProgramming the MSP430");
+	var child = exec(process.env.PB_ROOT + "/software/msp_images/flash_powerblade", function(error, stdout, stderr) {
+		if (error) throw error;
+		console.log("Done programming MSP430\n")
+		switch_to_calib();
+	});
+}
+
+/*************************************************************/
+// Step #3: Calibrate the PowerBlade
+/*************************************************************/
+function switch_to_calib() {
+	console.log("Preparing to calibrate. Move to the calibration rig.");
+	rl.question('Push enter when ready', function(answer) {
+		calibrate();
+	});
+}
+
+function calibrate() {
+	
 }
 
 db_connection.end();
