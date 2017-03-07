3c07cdd5241d8a0f4ef77f616dab433d0a94915b
Sam DeBruin <sdebruin@umich.edu>
2017-03-06 19:48:20 -0500

    updated upload status form

--- sql/upload_status_form.sql
+++ sql/upload_status_form.sql
@@ -1,5 +1,9 @@
 
-load data local infile 'C:/Users/Sam/Downloads/pb.csv' into table inf_pb_lookup fields terminated by ',' (deviceMAC, deviceName, category, deviceType, location, permanent, startTime, endTime);
-load data local infile 'C:/Users/Sam/Downloads/gw.csv' into table inf_gw_lookup fields terminated by ',' (gatewayMAC, location, startTime, endTime);
+#load data local infile 'C:/Users/Sam/Downloads/pb.csv' into table inf_pb_lookup fields terminated by ',' (deviceMAC, deviceName, category, deviceType, location, permanent, startTime, endTime);
+#load data local infile 'C:/Users/Sam/Downloads/gw.csv' into table inf_gw_lookup fields terminated by ',' (gatewayMAC, location, startTime, endTime);
 
+load data local infile '/Users/sdebruin/Downloads/PowerBlades.csv' into table inf_pb_lookup fields terminated by ',' (deviceMAC, deviceName, category, deviceType, permanent, room, location, startTime, endTime);
+load data local infile '/Users/sdebruin/Downloads/Gateways.csv' into table inf_gw_lookup fields terminated by ',' (gatewayMAC, room, location, startTime, endTime);
+load data local infile '/Users/sdebruin/Downloads/Blinks.csv' into table inf_blink_lookup fields terminated by ',' (deviceMAC, room, location, startTime, endTime);
+load data local infile '/Users/sdebruin/Downloads/BLEES.csv' into table inf_light_lookup fields terminated by ',' (deviceMAC, deviceName, power, deviceType, room, location, startTime, endTime);
 
