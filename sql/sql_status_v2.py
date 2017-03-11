11d797177056890c1d302edcc200f08c8e8bad7f
Sam DeBruin <sdebruin@umich.edu>
2017-03-11 11:31:57 -0500

    different th for ligeiro

--- sql/sql_status_v2.py
+++ sql/sql_status_v2.py
@@ -47,6 +47,9 @@ def check_devices(printLines, printOK, col1, col2, col3, list):
 			save_loc = location
 		if permanent == 1:
 			tot_count = tot_count + 1
+			countTh = 400
+			if(mac[6] == 'd'):
+				countTh = 5
 			if count > 400:
 				if printOK:
 					print_row(mac, name, location, STATUS_OK, count, '')
@@ -100,7 +103,7 @@ check_devices(True, False, 'deviceMAC', 'Room', 'Last Seen', success_blink)
 email_body.append("<tr><td colspan=\"5\">&nbsp</td></tr>")
 
 email_body.append("<tr><td colspan=\"5\"><b>BLEES & Ligeiro</b></td></tr>")
-check_devices(True, False, 'deviceMAC', 'Name', 'Last Seen', success_light)
+check_devices(True, True, 'deviceMAC', 'Name', 'Last Seen', success_light)
 email_body.append("<tr><td colspan=\"5\">&nbsp</td></tr>")
 
 email_body.append('</table></body></html>')
