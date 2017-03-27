9fe477a3fc1a148729139a93fbb6c5a88284fc88
Sam DeBruin <sdebruin@umich.edu>
2017-03-27 16:14:16 -0400

    add devtimepower to indexes

--- sql/add_indexes.sql
+++ sql/add_indexes.sql
@@ -3,6 +3,8 @@ alter table dat_powerblade add index devPower (timestamp, deviceMAC, power);
 
 alter table dat_powerblade add index devDevPower (deviceMAC, power, timestamp);
 
+alter table dat_powerblade add index devTimePower (deviceMAC, timestamp, power);
+
 alter table dat_blees add index devLux (deviceMAC, lux, timestamp);
 
 
