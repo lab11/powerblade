87c7c05e306287020f9dea9c8877fb59ed8a284a
Neal Jackson <nealjack@umich.edu>
2019-07-10 19:08:57 -0700

    Allow empty queue, prevent repeated waveform
    Don't fill queue if empty, keep track of most recent waveform until
    another is found.

--- software/nrf51822/apps/powerblade/waveforms.c
+++ software/nrf51822/apps/powerblade/waveforms.c
@@ -91,6 +91,7 @@ void flag_waveform_available(bool available, uint8_t* adv_data, uint16_t adv_len
 
 
 unique_circle_buf unique_waveforms = {0};
+unique_waveform_t most_recent_waveform = {0};
 
 void reset_waveforms(unique_circle_buf * buf) {
     buf->head = buf->tail = 0;
@@ -132,52 +133,56 @@ bool get_next_waveform(unique_waveform_t* data) {
     }
 }
 
+static bool compare_waveforms(unique_waveform_t* new, unique_waveform_t* compare) {
+    uint16_t real_power = parse_real_power(new->adv_payload, new->adv_len);
+    uint16_t apparent_power = parse_apparent_power(new->adv_payload, new->adv_len);
+    uint16_t compare_real_power = parse_real_power(compare->adv_payload, compare->adv_len);
+    uint16_t compare_app_power = parse_apparent_power(compare->adv_payload, compare->adv_len);
+    // approximate 10%
+    // is current waveform outside +/- 10% of other waveform?
+    uint16_t real_portion = compare_real_power >> 3;
+    uint16_t app_portion = compare_app_power >> 3;
+    if (real_power > compare_real_power + real_portion ||
+            real_power < compare_real_power - real_portion ||
+            apparent_power > compare_app_power + app_portion ||
+            apparent_power < compare_app_power - app_portion)
+    {
+        // great, this waveform's power is different!
+        // time to look at the next one
+        return true;
+    }
+    else {
+        // this waveform is pretty similar, let's not store this one
+        return false;
+    }
+
+}
+
 // determine if this waveform should be saved and do so.
 void check_and_store_new_waveform(uint8_t* waveform_data, uint16_t waveform_len, uint8_t* adv_data, uint16_t adv_len) {
     bool store = false;
+    static unique_waveform_t new_waveform;
+    new_waveform.adv_len = adv_len;
+    new_waveform.waveform_len= waveform_len;
+    memcpy(new_waveform.adv_payload,adv_data,adv_len);
+    memcpy(new_waveform.waveform_payload,waveform_data,waveform_len);
+
     // if full, ignore it
-    if (unique_waveforms.full) return;
-    // if empty, store it
-    if (unique_waveforms_empty(&unique_waveforms)) {
-        store = true;
+    if (unique_waveforms.full){
+        return;
     // else search and compare other existing waveforms
     // check based on sequence number, real and apparent power, and maybe some similarity metric
-    } else {
-        size_t i = unique_waveforms.tail;
-        store = true;
-        while (i != unique_waveforms.head) {
-            unique_waveform_t * compare_waveform = &unique_waveforms.data[i];
-            uint16_t real_power = parse_real_power(adv_data, adv_len);
-            uint16_t apparent_power = parse_real_power(adv_data, adv_len);
-            uint16_t compare_real_power = parse_real_power(compare_waveform->adv_payload, compare_waveform->adv_len);
-            uint16_t compare_app_power = parse_apparent_power(compare_waveform->adv_payload, compare_waveform->adv_len);
-            // approximate 10%
-            // is current waveform outside +/- 10% of other waveform?
-            uint16_t real_portion = compare_real_power >> 3;
-            uint16_t app_portion = compare_app_power >> 3;
-            if (real_power > compare_real_power + real_portion ||
-                   real_power < compare_real_power - real_portion ||
-                   apparent_power > compare_app_power + app_portion ||
-                   apparent_power < compare_app_power - app_portion)
-            {
-                // great, this waveform's power is different!
-                // time to look at the next one
-                i = (i + 1) % UNIQUE_WAVEFORM_MAX;
-                continue;
-            }
-            else {
-                // this waveform is pretty similar, let's not store this one
-                store = false;
-                break;
-            }
-        }
+    }
+    size_t i = unique_waveforms.tail;
+    store = false;
+    store = compare_waveforms(&new_waveform, &most_recent_waveform);
+    while (i != unique_waveforms.head && store) {
+        store = compare_waveforms(&new_waveform, &unique_waveforms.data[i]);
+        i = (i + 1) % UNIQUE_WAVEFORM_MAX;
     }
     if (store) {
-        size_t head = unique_waveforms.head;
-        unique_waveforms.data[head].adv_len = adv_len;
-        memcpy(unique_waveforms.data[head].adv_payload, adv_data, adv_len);
-        unique_waveforms.data[head].waveform_len = waveform_len;
-        memcpy(unique_waveforms.data[head].waveform_payload, waveform_data, waveform_len);
+        memcpy(&unique_waveforms.data[unique_waveforms.head], &new_waveform, sizeof(new_waveform));
+        memcpy(&most_recent_waveform, &new_waveform, sizeof(new_waveform));
         advance_pointer(&unique_waveforms);
     }
 }
