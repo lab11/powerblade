ef0cf17e09e6815dcc147620bec1283d77d284d2
Neal Jackson <nealjack@umich.edu>
2019-12-05 15:20:37 -0800

    add simple chroma library, works

---
+++ data_collection/multipoint_calibration/chroma63800.py
@@ -0,0 +1,55 @@
+import pyvisa
+
+class Chroma63800:
+    def __init__(self, device, peak_current=10):
+        self.rm = pyvisa.ResourceManager('@py')
+        self.inst = self.rm.open_resource(device, write_termination='\n', read_termination='\n', baud_rate=57600)
+        idn = self.inst.query('*IDN?')
+        if 'Chroma' not in idn and '63802' not in idn:
+            raise ValueError('You did not select a Chroma Load Emulator');
+        self.set_ipmax(peak_current)
+
+    def set_mode(self, mode):
+        if mode == 'power':
+            self.inst.write(':LOAD:MODE POW')
+        elif mode == 'current':
+            self.inst.write(':LOAD:MODE POW')
+    def set_ipmax(self, ipmax):
+        self.inst.write(':LOAD:CURR:PEAK:MAX ' + str(ipmax))
+    def set_cfpf_mode(self, mode):
+        if mode == 'cf':
+            self.inst.write(':SYST:SET:CFPF:MODE CF')
+        elif mode == 'pf':
+            self.inst.write(':SYST:SET:CFPF:MODE PF')
+        elif mode == 'both':
+            self.inst.write(':SYST:SET:CFPF:MODE BOTH')
+    def set_current(self, current):
+        self.set_mode('current')
+        self.inst.write(':LOAD:CURR ' + str(current))
+    def set_power(self, power):
+        self.set_mode('power')
+        self.inst.write(':LOAD:POW ' + str(power))
+    def set_pf(self, pf):
+        self.set_cfpf_mode('pf')
+        self.inst.write(':PFAC ' + str(pf))
+
+    def load_on(self):
+        self.inst.write(':LOAD 1')
+    def load_off(self):
+        self.inst.write(':LOAD 0')
+
+    def measure_voltage(self):
+        return float(self.inst.query(':MEAS:VOLT?'))
+    def measure_current(self):
+        return float(self.inst.query(':MEAS:CURR?'))
+    def measure_apparent_power(self):
+        return float(self.inst.query(':MEAS:POW:APP?'))
+    def measure_reactive_power(self):
+        return float(self.inst.query(':MEAS:POW:REAC?'))
+    def measure_real_power(self):
+        return float(self.inst.query(':MEAS:POW:REAL?'))
+    def measure_pf(self):
+        return float(self.inst.query(':MEAS:POW:PFAC?'))
+    def measure_all(self):
+        return (self.measure_voltage(), self.measure_current(), self.measure_apparent_power(), self.measure_real_power(), self.measure_reactive_power(), self.measure_pf())
+
--- data_collection/multipoint_calibration/generate_calibration_curve.py
+++ data_collection/multipoint_calibration/generate_calibration_curve.py
@@ -1,18 +1,52 @@
 import pyvisa
+import time
+import argparse
+from chroma63800 import Chroma63800
 
-rm = pyvisa.ResourceManager()
-resources = rm.list_resources()
-print("Select resource:")
-inst = None
-while not (choice < len(resources) and choice >= 0):
-    for i, resource in enumerate(resources):
-        print(str(i+1) + '.', resource)
-    choice = int(input("Selection: ")) - 1
-    if not (choice < len(resources) and choice >= 0): print("Invalid selection")
-    else:
-        inst = rm.open_resource(resources[choice], read_terminator='\n', baud_rate=57600)
-        print('Selected: ', inst)
-        idn = inst.query('*IDN?')
-        if 'Chroma' not in idn and '63802' not in idn:
-            print('You did not select a Chroma Load Emulator');
-print('outside')
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--device", help="VISA device string to connect to")
+args = parser.parse_args()
+device = None
+if args.device is not None:
+    device = args.device
+else:
+    rm = pyvisa.ResourceManager()
+    resources = rm.list_resources()
+    print("Select resource:")
+    inst = None
+    choice = -1
+    while not (choice < len(resources) and choice >= 0):
+        for i, resource in enumerate(resources):
+            print(str(i+1) + '.', resource)
+        choice = int(input("Selection: ")) - 1
+        if not (choice < len(resources) and choice >= 0): print("Invalid selection")
+        else:
+            device = resources[choice]
+
+chroma = Chroma63800(device, 15)
+
+chroma.set_power(1000)
+chroma.set_pf(1)
+chroma.load_on()
+time.sleep(5)
+print(chroma.measure_all())
+time.sleep(10)
+chroma.load_off()
+#inst.write(':SYST:SET:CFPF:MODE PF')
+#inst.write(':LOAD:ABA ON')
+#inst.write(':LOAD:CURR:PEAK:MAX 10')
+#print(inst.query(':LOAD?'))
+#inst.write(':LOAD:MODE POW')
+#inst.write(':PFAC .5')
+#print(inst.query(':PFAC?'))
+#print(inst.query(':LOAD:MODE?'))
+#inst.write(':LOAD:POW 75')
+#inst.write(':LOAD ON')
+#
+#time.sleep(5)
+#print(inst.query(':MEAS:POW?'))
+#print(inst.query(':MEAS:POW:PFAC?'))
+#print(inst.query(':MEAS:VOLT?'))
+#time.sleep(5)
+#
+#inst.write(':LOAD OFF')
